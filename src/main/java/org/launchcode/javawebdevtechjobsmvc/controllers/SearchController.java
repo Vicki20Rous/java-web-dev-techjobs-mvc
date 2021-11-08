0575d31ee0ad5c45f62da4f125fac255abf24ef4
Vicki20rous <vic86rous@gmail.com>
2021-11-08 11:25:14 -0600

    added new handler

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
@@ -24,5 +25,16 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+        @RequestMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+             if (searchTerm.toLowerCase().equals("all") || searchTerm.isEmpty()){
+                jobs = JobData.findAll();
+            } else {
+                jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            }
+            model.addAttribute("jobs", jobs);
 
+            return "search";
+        }
 }
